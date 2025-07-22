ebfcb327af98463006873e24284bd31b688647c3
Aravind <aravind19112003@gmail.com>
2025-07-22 12:55:00 +0530

    decouple listener logic

---
+++ job-orchestrator/queue/listener.go
@@ -0,0 +1,59 @@
+package queue
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func Listen() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// Create a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// Declare (or connect to) a queue
+	q, err := ch.QueueDeclare(
+		"webhook.queue", // name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// Consume messages from the queue
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-acknowledge
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // arguments
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	// Create a channel to block forever
+	forever := make(chan bool)
+
+	// Print incoming messages
+	go func() {
+		for d := range msgs {
+			log.Printf("Received message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
