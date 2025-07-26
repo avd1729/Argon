e731f394d6dfd6b200b072245668e6c403e475d6
Aravind <aravind19112003@gmail.com>
2025-07-26 17:42:52 +0530

    refactor listener

--- notification-service/src/listener.py
+++ notification-service/src/listener.py
@@ -1,48 +1,42 @@
 import pika
 from src.email_service import send_email
 
-def listen(queue_name='notification.queue', host='localhost'):
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBIT_URL = os.getenv("RABBIT_MQ_LISTENER_URL")
+NOTIFICATION_QUEUE = "notification.queue"
+
+
+def listen():
     def callback(ch, method, properties, body):
         try:
             send_email(body.decode())
+            print("[x] Email sent:", body.decode())
         except Exception as e:
             print(f"[!] Error processing message: {e}")
 
-    # Connect to RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-    channel = connection.channel()
+    def connect_and_consume():
+        connection = pika.BlockingConnection(pika.URLParameters(RABBIT_URL))
+        channel = connection.channel()
+        channel.queue_declare(queue=NOTIFICATION_QUEUE, durable=True)
 
-    # Ensure the queue exists
-    channel.queue_declare(queue=queue_name)
+        channel.basic_consume(
+            queue=NOTIFICATION_QUEUE,
+            on_message_callback=callback,
+            auto_ack=True
+        )
 
-    # Start consuming messages
-    channel.basic_consume(
-        queue=queue_name,
-        on_message_callback=callback,
-        auto_ack=True 
-    )
+        print(f"[*] Listening on queue '{NOTIFICATION_QUEUE}'. Press CTRL+C to stop.")
+        channel.start_consuming()
 
-    print(f"[*] Listening on queue '{queue_name}'. Press CTRL+C to stop.")
     while True:
         try:
-            channel.start_consuming()
+            connect_and_consume()
         except KeyboardInterrupt:
             print("\n[!] Stopped by user")
             break
         except Exception as e:
-            print(f"[!] Listener crashed with: {e}. Restarting...")
-            try:
-                connection.close()
-            except:
-                pass
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-            channel = connection.channel()
-            channel.queue_declare(queue=queue_name)
-            channel.basic_consume(
-                queue=queue_name,
-                on_message_callback=callback,
-                auto_ack=True
-            )
-
-    channel.stop_consuming()
-    connection.close()
+            print(f"[!] Listener crashed: {e}. Reconnecting...")
